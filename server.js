f77dec570497a6de1bf18024d709884c2c5c4c9c
Ishan Gajurel <ishan.gajurel@verscend.com>
2019-11-12 22:18:47 -0600

    'all'

---
+++ server.js
@@ -0,0 +1,42 @@
+//this is the server node.js file
+//to load http module,create a server, open a port and listen on it
+//accept client request and send response
+
+var http = require('http');
+var fs = require('fs');
+var querystring = require('querystring');
+
+http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type':'text/html'});
+    let x=''
+    //response.end('Hello World from GajjuBhai.com!\n');
+    fs.readFile('./index.html',null, function(error, data){
+        if(error){
+            response.writeHead(404);
+            response.write('Error found 404 in this app-dig now');
+        }
+        else{
+            response.write(data);
+            if(request.method == 'POST'){ // && request.url == './index.html'
+                let body = '';
+                let x = '';
+
+                request.on('data', chunk =>{
+                    body += chunk.toString();
+                });
+
+                
+                request.on('end', () =>{
+                    body = querystring.parse(body);
+                    console.log(body);
+                    x += JSON.stringify(body['user[name]']);
+                    response.end(x);
+                });
+            }
+        }
+        //response.end('To Be Continued from GajjuBhai.com 123!\n');
+    });
+}).listen(9080);
+
+console.log('Server running at http://127.0.0.1:9080/');
+
---
+++ sortage.js
@@ -0,0 +1,7 @@
+//import {PythonShell} from 'python-shell'
+
+let {PythonShell} = require('python-shell')
+
+//import {PythonShell} from 'python-shell';
+
+//var x = require('python-shell');
